d305ecce59fcefaea5493ae8f4a24c1e7da3292f
Fatih Doğan <fth.dgn.369@gmail.com>
2022-05-18 22:23:28 +0300

    Added image draw to sample.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { run, signalExit } from './run';
-
+function filePathFromApp(path: string) { return `app0:/${path}`; }
 run({
-  init: () => { 
+  init: () => {
     return {
       text: "Hello world - Press ENTER to close.",
       textPosition: {
@@ -9,11 +9,18 @@ run({
         y: 5
       },
       textColor: Color.new(255, 255, 255),
-      enterCtrl: Controls.getEnterButton()
-    } 
+      enterCtrl: Controls.getEnterButton(),
+
+      image: Graphics.loadImage(filePathFromApp("assets/100x100.png")),
+      imagePosition: {
+        x: 5,
+        y: 50
+      }
+    }
   },
   draw: (state) => {
     Graphics.debugPrint(state.textPosition.x, state.textPosition.y, state.text, state.textColor);
+    Graphics.drawImage(state.imagePosition.x, state.imagePosition.y, state.image);
   },
   check: (state) => {
     let pad = Controls.read();
